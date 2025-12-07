fbb22ed04b6ab5bb27b39c8e94b605578215bcea
XerWo <realgamerex@gmail.com>
2025-12-07 03:05:12 +0100

    Added zep.run url

--- src/constants/default.zig
+++ src/constants/default.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub const zig_download_index = "https://ziglang.org/download/index.json";
+pub const zep_download_index = "https://zep.run/download.json";
 pub const kb = 1024;
 pub const mb = kb * kb;
 
