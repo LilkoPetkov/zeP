7c0e73c1d51e281a6e599ddc3d748d869ec516ad
XerWo <realgamerex@gmail.com>
2025-12-07 02:38:12 +0100

    installed clap@0.10.0

--- .zep/injector.zig
+++ .zep/injector.zig
@@ -1,2 +1,9 @@
 const std = @import("std");
-pub fn injectExtraImports(_: *std.Build, _: *std.Build.Step.Compile) void {}
+pub fn injectExtraImports(b: *std.Build, exe: *std.Build.Step.Compile) void { 
+ // clap MODULE 
+ const clapMod = b.createModule(.{ 
+ .root_source_file = b.path(".zep/clap/clap.zig"), 
+ }); 
+ exe.root_module.addImport("clap", clapMod); 
+ // ---------- 
+}
