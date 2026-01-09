781c76b7177d2c878ccf0fc27f4925a0947d9df2
XerWo <realgamerex@gmail.com>
2026-01-09 22:28:08 +0100

    removed important folder or file filters

--- src/constants/extras.zig
+++ src/constants/extras.zig
@@ -10,32 +10,9 @@ pub const package_files = struct {
 pub const filtering = struct {
     pub const folders = [_][]const u8{
         ".git",
-        ".github",
         ".vscode",
         ".zig-cache",
-        ".docker",
-        "example",
-        "examples",
-        "test",
-        "tests",
-        "testdata",
-        "docker",
-        "doc",
-        "docs",
-        "cmake",
     };
 
-    pub const files = [_][]const u8{
-        ".editorconfig",
-        ".gitignore",
-        ".gitattributes",
-        ".travis.yml",
-        "travis.yml",
-        "LICENSE",
-        "README.md",
-        "readme.md",
-        "todo.md",
-        "test.zig",
-        "license.txt",
-    };
+    pub const files = [_][]const u8{};
 };
