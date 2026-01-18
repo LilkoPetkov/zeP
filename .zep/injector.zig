8387fb679652f453670019c7704cb5becf0c0383
XerWo <realgamerex@gmail.com>
2026-01-18 19:08:39 +0100

    re-installation quirks

--- .zep/injector.zig
+++ .zep/injector.zig
@@ -1,21 +1,21 @@
 const std = @import("std");
 pub fn imp(b: *std.Build, exe: *std.Build.Module) void {
-    // clap MODULE
-    const clapMod = b.createModule(.{
-        .root_source_file = b.path(".zep/clap/clap.zig"),
-    });
-    exe.addImport("clap", clapMod);
-    // ----------
-    // mvzr MODULE
-    const mvzrMod = b.createModule(.{
-        .root_source_file = b.path(".zep/mvzr/src/mvzr.zig"),
-    });
-    exe.addImport("mvzr", mvzrMod);
-    // ----------
-    // logly MODULE
-    const loglyMod = b.createModule(.{
-        .root_source_file = b.path(".zep/logly/src/logly.zig"),
-    });
-    exe.addImport("logly", loglyMod);
-    // ----------
+ // logly MODULE
+ const loglyMod = b.createModule(.{
+     .root_source_file = b.path(".zep/logly/src/logly.zig"),
+ });
+ exe.addImport("logly", loglyMod);
+ // ----------
+ // mvzr MODULE
+ const mvzrMod = b.createModule(.{
+     .root_source_file = b.path(".zep/mvzr/src/mvzr.zig"),
+ });
+ exe.addImport("mvzr", mvzrMod);
+ // ----------
+ // clap MODULE
+ const clapMod = b.createModule(.{
+     .root_source_file = b.path(".zep/clap/clap.zig"),
+ });
+ exe.addImport("clap", clapMod);
+ // ----------
 }
