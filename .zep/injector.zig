e3f3a9651cb1c47100132644f8cad1bdfa9f8b76
XerWo <realgamerex@gmail.com>
2026-01-24 22:36:10 +0100

    re-installation quirks

--- .zep/injector.zig
+++ .zep/injector.zig
@@ -1,15 +1,15 @@
 const std = @import("std");
 pub fn imp(b: *std.Build, exe: *std.Build.Module) void {
- // mvzr MODULE
- const mvzrMod = b.createModule(.{
-     .root_source_file = b.path(".zep/mvzr/src/mvzr.zig"),
- });
- exe.addImport("mvzr", mvzrMod);
- // ----------
  // logly MODULE
  const loglyMod = b.createModule(.{
      .root_source_file = b.path(".zep/logly/src/logly.zig"),
  });
  exe.addImport("logly", loglyMod);
+ // ----------
+ // mvzr MODULE
+ const mvzrMod = b.createModule(.{
+     .root_source_file = b.path(".zep/mvzr/src/mvzr.zig"),
+ });
+ exe.addImport("mvzr", mvzrMod);
  // ----------
 }
