88f76162e9af6344725e64ac463d8091df4763c0
XerWo <realgamerex@gmail.com>
2026-02-08 04:42:32 +0100

    injector importing change

--- .zep/injector.zig
+++ .zep/injector.zig
@@ -1,16 +1,16 @@
 const std = @import("std");
 pub fn imp(b: *std.Build, exe: *std.Build.Module) void {
- // zstd.zig MODULE
- const zstd_dep = b.dependency("zstd", .{});
- exe.addImport("zstd", zstd_dep.module("zstd"));
+ // zon.zig MODULE
+ const zon_dep = b.dependency("zon", .{});
+ exe.addImport("zon", zon_dep.module("zon"));
  // ----------
  // logly.zig MODULE
  const logly_dep = b.dependency("logly", .{});
  exe.addImport("logly", logly_dep.module("logly"));
  // ----------
- // zon.zig MODULE
- const zon_dep = b.dependency("zon", .{});
- exe.addImport("zon", zon_dep.module("zon"));
+ // zstd.zig MODULE
+ const zstd_dep = b.dependency("zstd", .{});
+ exe.addImport("zstd", zstd_dep.module("zstd"));
  // ----------
  // mvzr MODULE
  const mvzr_dep = b.dependency("mvzr", .{});
