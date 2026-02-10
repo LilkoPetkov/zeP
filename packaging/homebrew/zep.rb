8b24df9b8ace8a851f654b5613a5dcc5d6d24fd9
XerWo <realgamerex@gmail.com>
2026-02-10 20:57:01 +0100

    fixed packaging

--- packaging/homebrew/zep.rb
+++ packaging/homebrew/zep.rb
@@ -4,13 +4,13 @@ class Zep < Formula
   license "GPLv3"
 
   on_macos do
-    url "https://zep.run/releases/1.1.0/zep_x86_64-macos_1.1.0.tar.xz"
-    sha256 "ab5a1e16e8f431316a4cb1c571d26ce4d435d2a29caa766381984e65be992b24"
+    url "https://zep.run/releases/1.2.0/zep_x86_64-macos_1.2.0.tar.xz"
+    sha256 "24f3cf9a844254bbb37601010eea618343b80f09d345939dc3fb99ebcdc0995d"
   end
 
   on_linux do
-    url "https://zep.run/releases/1.1.0/zep_x86_64-linux_1.1.0.tar.xz"
-    sha256 "5ad4cf09545abb7ca7d52c61441108c0022459ade91d7d938f5eca012e0ed4c6"
+    url "https://zep.run/releases/1.2.0/zep_x86_64-linux_1.2.0.tar.xz"
+    sha256 "4713b33b59e0fe627e6aebe5ab202043da83c32b76fd710262b1c265efd28616"
   end
 
   def install
